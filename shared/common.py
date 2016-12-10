b40b3adc9ac4b7404233d3bada1b24e47ec1e745
Pascal Mathis <git@development.smx.li>
2016-12-10 18:15:00 +0100

    [check_xmpp] Added module to monitor XMPP servers (connection setup, StartTLS, certificate expiry, ...)

--- shared/common.py
+++ shared/common.py
@@ -1,6 +1,37 @@
 import nagiosplugin
 
 
+class DaysValidContext(nagiosplugin.Context):
+    fmt_hint = 'less than {value} days'
+
+    def __init__(self, name, check_lifetime=True, warning_days=0, critical_days=0, fmt_metric='Valid for {value} days'):
+        super(DaysValidContext, self).__init__(name, fmt_metric=fmt_metric)
+
+        self.name = name
+        self.check_lifetime = check_lifetime
+        self.warning_days = warning_days
+        self.critical_days = critical_days
+
+        self.warning = nagiosplugin.Range('@%d:' % self.warning_days)
+        self.critical = nagiosplugin.Range('@%d:' % self.critical_days)
+
+    def evaluate(self, metric, resource):
+        if self.check_lifetime and metric.value is not None:
+            if self.critical.match(metric.value):
+                return nagiosplugin.Result(nagiosplugin.Critical, hint=self.fmt_hint.format(value=self.critical_days), metric=metric)
+            elif self.warning.match(metric.value):
+                return nagiosplugin.Result(nagiosplugin.Warn, hint=self.fmt_hint.format(value=self.warning_days), metric=metric)
+            else:
+                return super(DaysValidContext, self).evaluate(metric, resource)
+        else:
+            return super(DaysValidContext, self).evaluate(metric, resource)
+
+    def performance(self, metric, resource):
+        if self.check_lifetime and metric.value is not None:
+            return nagiosplugin.Performance(self.name, metric.value, '')
+        return None
+
+
 class ExceptionContext(nagiosplugin.Context):
     def __init__(self, name):
         super(ExceptionContext, self).__init__(name)
