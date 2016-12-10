d685a51904e63d19b5b1c2a0ded2a06c32acfbbb
Pascal Mathis <git@development.smx.li>
2016-12-10 18:31:44 +0100

    [check_xmpp] Improved output when using --no-check-certificates

--- shared/common.py
+++ shared/common.py
@@ -1,6 +1,19 @@
 import nagiosplugin
 
 
+class CommaSeparatedSummary(nagiosplugin.Summary):
+    def ok(self, results):
+        return ', '.join([str(result) for result in results if result.metric])
+
+
+class ExceptionContext(nagiosplugin.Context):
+    def __init__(self, name):
+        super(ExceptionContext, self).__init__(name)
+
+    def evaluate(self, metric, resource):
+        return self.result_cls(nagiosplugin.Critical, str(metric.value), metric)
+
+
 class DaysValidContext(nagiosplugin.Context):
     fmt_hint = 'less than {value} days'
 
@@ -18,23 +31,17 @@ class DaysValidContext(nagiosplugin.Context):
     def evaluate(self, metric, resource):
         if self.check_lifetime and metric.value is not None:
             if self.critical.match(metric.value):
-                return nagiosplugin.Result(nagiosplugin.Critical, hint=self.fmt_hint.format(value=self.critical_days), metric=metric)
+                return nagiosplugin.Result(nagiosplugin.Critical, hint=self.fmt_hint.format(value=self.critical_days),
+                                           metric=metric)
             elif self.warning.match(metric.value):
-                return nagiosplugin.Result(nagiosplugin.Warn, hint=self.fmt_hint.format(value=self.warning_days), metric=metric)
+                return nagiosplugin.Result(nagiosplugin.Warn, hint=self.fmt_hint.format(value=self.warning_days),
+                                           metric=metric)
             else:
                 return super(DaysValidContext, self).evaluate(metric, resource)
         else:
-            return super(DaysValidContext, self).evaluate(metric, resource)
+            return nagiosplugin.Result(nagiosplugin.Ok)
 
     def performance(self, metric, resource):
         if self.check_lifetime and metric.value is not None:
             return nagiosplugin.Performance(self.name, metric.value, '')
         return None
-
-
-class ExceptionContext(nagiosplugin.Context):
-    def __init__(self, name):
-        super(ExceptionContext, self).__init__(name)
-
-    def evaluate(self, metric, resource):
-        return self.result_cls(nagiosplugin.Critical, str(metric.value), metric)
